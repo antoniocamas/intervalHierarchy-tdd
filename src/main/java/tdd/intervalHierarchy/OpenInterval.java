5963344050cc498f725dc25090c207c91ba643aa
Antonio Camas <antoniocamas@hotmail.com>
2019-05-30 19:28:20 +0200

    first commit

---
+++ src/main/java/tdd/intervalHierarchy/OpenInterval.java
@@ -0,0 +1,28 @@
+package tdd.intervalHierarchy;
+
+public class OpenInterval {
+
+	private double min;
+	
+	private double max;
+
+	public OpenInterval(double min, double max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	public boolean isIntersected(OpenInterval another) {
+		if(another.min == another.max || this.min == this.max) {
+			return false;
+		}
+		return this.isIncluded(another.min) ||
+				this.isIncluded(another.max)||
+				another.isIncluded(this.min) || this.min == another.min;
+	}
+
+	private boolean isIncluded(double value) {
+		return this.min < value && value < this.max;
+	}
+
+}
+
